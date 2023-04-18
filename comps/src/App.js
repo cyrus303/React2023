c98c7abfd59dd4b33389312cdbd50088f52739e0
cyrus303 <sachinsunny303@gmail.com>
2023-04-18 07:10:59 +0100

    accordion completed

--- comps/src/App.js
+++ comps/src/App.js
@@ -5,17 +5,20 @@ function App() {
     {
       id: 1,
       lable: 'Can I Use React on a Project',
-      content: 'You can use React on any project you want',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, minima iusto similique vitae amet a illo adipisci deserunt, quod quasi ipsum itaque iste explicabo quia, at magni atque sunt consectetur',
     },
     {
       id: 2,
       lable: 'Can I Use TypeScript on a Project',
-      content: 'You can use TypeScript on any project you want',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, minima iusto similique vitae amet a illo adipisci deserunt, quod quasi ipsum itaque iste explicabo quia, at magni atque sunt consectetur',
     },
     {
       id: 3,
       lable: 'Can I Use CSS on a Project',
-      content: 'You can use CSS on any project you want',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, minima iusto similique vitae amet a illo adipisci deserunt, quod quasi ipsum itaque iste explicabo quia, at magni atque sunt consectetur',
     },
   ];
 
--- comps/src/components/Accordion.js
+++ comps/src/components/Accordion.js
@@ -1,16 +1,44 @@
+import { useState } from 'react';
+import { AiFillCaretDown, AiFillCaretLeft } from 'react-icons/ai';
+
 function Accordion({ items }) {
-  const rendrendItems = items.map((item) => {
+  const [expandedIndex, setExpandedIndex] = useState(-1);
+
+  const handleClick = (index) => {
+    if (index === expandedIndex) {
+      setExpandedIndex(-1);
+    } else {
+      setExpandedIndex(index);
+    }
+  };
+
+  const rendrendItems = items.map((item, index) => {
     const { lable, content, id } = item;
+    const isExpanded = index === expandedIndex;
+
+    const icon = (
+      <span className="text-xl">
+        {isExpanded ? <AiFillCaretDown /> : <AiFillCaretLeft />}
+      </span>
+    );
+
     return (
       <div key={id}>
-        <h1>{lable}</h1>
-        <p>{content}</p>
+        <div
+          className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer"
+          onClick={() => {
+            handleClick(index);
+          }}
+        >
+          {lable}
+          {icon}
+        </div>
+        {isExpanded && <div className="border-b p-5">{content}</div>}
       </div>
     );
   });
 
-  //   console.log(structure);
-  return <div>{rendrendItems}</div>;
+  return <div className="border-x border-t rounded">{rendrendItems}</div>;
 }
 
 export default Accordion;
