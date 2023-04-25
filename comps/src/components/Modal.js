64ebec0772b1030302fddc9ce88663959fad14a1
cyrus303 <sachinsunny303@gmail.com>
2023-04-25 07:40:09 +0100

    modal completed

--- comps/src/components/Modal.js
+++ comps/src/components/Modal.js
@@ -1,5 +1,34 @@
-function Modal() {
-  return <div>hello from modal</div>;
+import ReactDOM from 'react-dom';
+import { useEffect } from 'react';
+
+function Modal({ onClose, children, actionBar }) {
+  useEffect(() => {
+    document.body.classList.add('overflow-hidden');
+
+    return () => {
+      document.body.classList.remove('overflow-hidden');
+    };
+  }, []);
+
+  const handleClick = () => {
+    onClose();
+  };
+
+  return ReactDOM.createPortal(
+    <div>
+      <div
+        className="fixed inset-0 bg-gray-300 opacity-80"
+        onClick={handleClick}
+      ></div>
+      <div className="fixed inset-40 p-10 bg-white">
+        <div className="flex flex-col justify-between h-full">
+          {children}
+          <div className="flex justify-end">{actionBar}</div>
+        </div>
+      </div>
+    </div>,
+    document.querySelector('.modal-container')
+  );
 }
 
 export default Modal;
--- comps/src/pages/ModalPage.js
+++ comps/src/pages/ModalPage.js
@@ -1,9 +1,157 @@
 import Modal from '../components/Modal';
+import Button from '../components/Button';
+import { useState } from 'react';
 
 function ModalPage() {
+  const [showModal, setShowModal] = useState(false);
+  const handleClick = () => {
+    setShowModal(true);
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
+  const actionBar = (
+    <div>
+      <Button onClick={handleClose} primary>
+        I Accept
+      </Button>
+    </div>
+  );
+
+  const modal = (
+    <Modal onClose={handleClose} actionBar={actionBar}>
+      <p>Here is an important agreement for you to accept</p>
+    </Modal>
+  );
+
   return (
     <div>
-      <Modal />
+      <Button onClick={handleClick} primary>
+        Open Modal
+      </Button>
+      {showModal && modal}
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
+        tempora nam accusamus quia provident cumque dolorum velit autem minima
+        eaque excepturi voluptatem explicabo, cum placeat eligendi, dolores quae
+        voluptatibus mollitia?
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
+        tempora nam accusamus quia provident cumque dolorum velit autem minima
+        eaque excepturi voluptatem explicabo, cum placeat eligendi, dolores quae
+        voluptatibus mollitia?
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
+        tempora nam accusamus quia provident cumque dolorum velit autem minima
+        eaque excepturi voluptatem explicabo, cum placeat eligendi, dolores quae
+        voluptatibus mollitia?
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
+        tempora nam accusamus quia provident cumque dolorum velit autem minima
+        eaque excepturi voluptatem explicabo, cum placeat eligendi, dolores quae
+        voluptatibus mollitia?
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
+        tempora nam accusamus quia provident cumque dolorum velit autem minima
+        eaque excepturi voluptatem explicabo, cum placeat eligendi, dolores quae
+        voluptatibus mollitia?
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
+        tempora nam accusamus quia provident cumque dolorum velit autem minima
+        eaque excepturi voluptatem explicabo, cum placeat eligendi, dolores quae
+        voluptatibus mollitia?
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
+        tempora nam accusamus quia provident cumque dolorum velit autem minima
+        eaque excepturi voluptatem explicabo, cum placeat eligendi, dolores quae
+        voluptatibus mollitia?
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
+        tempora nam accusamus quia provident cumque dolorum velit autem minima
+        eaque excepturi voluptatem explicabo, cum placeat eligendi, dolores quae
+        voluptatibus mollitia?
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
+        tempora nam accusamus quia provident cumque dolorum velit autem minima
+        eaque excepturi voluptatem explicabo, cum placeat eligendi, dolores quae
+        voluptatibus mollitia?
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
+        tempora nam accusamus quia provident cumque dolorum velit autem minima
+        eaque excepturi voluptatem explicabo, cum placeat eligendi, dolores quae
+        voluptatibus mollitia?
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
+        tempora nam accusamus quia provident cumque dolorum velit autem minima
+        eaque excepturi voluptatem explicabo, cum placeat eligendi, dolores quae
+        voluptatibus mollitia?
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
+        tempora nam accusamus quia provident cumque dolorum velit autem minima
+        eaque excepturi voluptatem explicabo, cum placeat eligendi, dolores quae
+        voluptatibus mollitia?
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
+        tempora nam accusamus quia provident cumque dolorum velit autem minima
+        eaque excepturi voluptatem explicabo, cum placeat eligendi, dolores quae
+        voluptatibus mollitia?
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
+        tempora nam accusamus quia provident cumque dolorum velit autem minima
+        eaque excepturi voluptatem explicabo, cum placeat eligendi, dolores quae
+        voluptatibus mollitia?
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
+        tempora nam accusamus quia provident cumque dolorum velit autem minima
+        eaque excepturi voluptatem explicabo, cum placeat eligendi, dolores quae
+        voluptatibus mollitia?
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
+        tempora nam accusamus quia provident cumque dolorum velit autem minima
+        eaque excepturi voluptatem explicabo, cum placeat eligendi, dolores quae
+        voluptatibus mollitia?
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
+        tempora nam accusamus quia provident cumque dolorum velit autem minima
+        eaque excepturi voluptatem explicabo, cum placeat eligendi, dolores quae
+        voluptatibus mollitia?
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
+        tempora nam accusamus quia provident cumque dolorum velit autem minima
+        eaque excepturi voluptatem explicabo, cum placeat eligendi, dolores quae
+        voluptatibus mollitia?
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
+        tempora nam accusamus quia provident cumque dolorum velit autem minima
+        eaque excepturi voluptatem explicabo, cum placeat eligendi, dolores quae
+        voluptatibus mollitia?
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
+        tempora nam accusamus quia provident cumque dolorum velit autem minima
+        eaque excepturi voluptatem explicabo, cum placeat eligendi, dolores quae
+        voluptatibus mollitia?
+      </p>
     </div>
   );
 }
